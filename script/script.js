526d498645b9f7ba6b2d29411be442328fb59548
marconius <marco@cuthemustard.com>
2014-04-05 14:19:50 -0400

    override au lieu de remplacer le css des skins

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
 
 function changeCss(id){ 
-  var stl = id;     // sets the css file, according to plusmin
-
   // change the css file of the tag with id="stl" and rel="stylesheet"
-  $('#stl[rel=stylesheet]').attr('href', stl);
+  $('#stl[rel=stylesheet]').attr('href', id);
 }
