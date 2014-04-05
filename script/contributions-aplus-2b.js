d2eca631d826f1fc234e66e646cdb4a952a181fb
David Tucker <davidt2003@msn.com>
2014-04-05 11:55:45 -0400

    Fonctionnalités 2b

---
+++ script/contributions-aplus-2b.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+	test();
+});
+
+function test() {
+  getRevisionsBefore("25227114", "602185304", function(revisions) {
+	  alert("Nb contributions avant : " + revisions.length);
+  });
+
+  getRevisionsAfter("25227114", "602185304", function(revisions) {
+	  alert("Nb contributions apres : " + revisions.length);
+  });
+}
+
+function getRevisionsBefore(pageid, revid, complete) {
+	getAllRevisions(pageid, function(revisions) {
+		var results = [];
+		for (var i = 0; i < revisions.length; ++i) {
+			if (revisions[i].revid == revid) {
+				break;
+			}
+			results.push(revisions[i]);
+		}
+		complete(results);
+	});
+}
+
+function getRevisionsAfter(pageid, revid, complete) {
+	getAllRevisions(pageid, function(revisions) {
+		var results = [];
+		var passed = false;
+		for (var i = 0; i < revisions.length; ++i) {
+			if (passed) {
+				results.push(revisions[i]);
+			}
+
+			if (revisions[i].revid == revid) {
+				passed = true;
+			}
+		}
+		complete(results);
+	});
+}
+
+function getAllRevisions(pageid, complete) {
+	$.ajax({
+		url: getUrlForAllRevisions(pageid),
+		dataType: "jsonp",
+		type: 'GET',
+		success: function(response) {
+			complete(response.query.pages[pageid].revisions);
+		}
+	});
+}
+
+function getUrlForAllRevisions(pageid) {
+	return "http://en.wikipedia.org/w/api.php?" +
+		"format=json&" +
+		"action=query&" +
+		"pageids=" + escape(pageid) + "&" +
+		"prop=revisions&" +
+		"rvprop=ids&" +
+		"rvlimit=max";
+}
